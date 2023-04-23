db62daeb0ba7961d6b1a0a21e13e7a5599c95cc0
akashkr11668 <akash11668@gmail.com>
2023-04-23 08:55:17 +0530

    REST API CRUD COMPLETED

---
+++ middleware/errorHandler.js
@@ -0,0 +1,44 @@
+const {constants} = require("../constants")
+const errorHandler = (err, req, res, next) =>{
+      const statusCode = res.statusCode ? res.statusCode : 500;
+      switch (statusCode) {
+            case constants.VALIDATION_ERROR:
+                  res.json({
+                        title: "Validation Failed",
+                        message: err.message, 
+                        stackTrace: err.stack,
+                   })
+      break;
+            case constants.NOT_FOUND:
+                  res.json(
+                        {title: "Not found",
+                        message: err.message,
+                        stackTrace: err.stack,
+                   })
+            case constants.UNAUTHORIZED:
+                  res.json(
+                        {title: "UNAUTHORIZED",
+                        message: err.message,
+                        stackTrace: err.stack,
+                   }) 
+            case constants.FORBIDDEN:
+                   res.json(
+                       {title: "FORBIDDEN",
+                        message: err.message,
+                        stackTrace: err.stack,
+                   })
+            case constants.SERVER_ERROR:
+                  res.json(
+                        {title: "Server Err",
+                        message: err.message,
+                        stackTrace: err.stack,
+                  })                                        
+            default:
+                  console.log("No error, Ok")
+            break;
+      }
+     
+      
+}
+
+module.exports= errorHandler;
